e69eab4c61d634a724c3765b52f8cbdef5b95bf4
Brown Memoye <memoyebrown@gmail.com>
2024-06-26 02:07:14 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
-import { NotesContextProvider } from "./context/NoteContextProvider.tsx";
+import { NotesContextProvider } from "./context/NotesContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
